e2569a08b2506285de13aad7d39eca5fb408e4f1
Kayleb Aubie Boudreau <boudreaukayleb@gmail.com>
2026-04-10 12:45:44 -0300

    avec snapshot pour le comparatif

--- backend/Migrations/AppDbContextModelSnapshot.cs
+++ backend/Migrations/AppDbContextModelSnapshot.cs
@@ -2,7 +2,6 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using backend.Infrastructure;
@@ -12,11 +11,9 @@ using backend.Infrastructure;
 namespace backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260410151319_AutoMigration_20260410151300")]
-    partial class AutoMigration_20260410151300
+    partial class AppDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
