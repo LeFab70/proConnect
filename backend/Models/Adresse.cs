54e2a48bcadad519fd2fa3e3b2284de20a3adb90
LeFab70 <kouonang2002@gmail.com>
2026-05-08 11:00:57 -0300

    fix: mark required address fields

--- backend/Models/Adresse.cs
+++ backend/Models/Adresse.cs
@@ -3,9 +3,9 @@ namespace backend.Models;
 
 public class Adresse
 {
-    public string Numero { get; set; } // Numéro de rue de l'adresse
-    public string Rue { get; set; } // Rue de l'adresse
-    public string Ville { get; set; } // Ville de l'adresse
-    public string CodePostal { get; set; } // Code postal de l'adresse
-    public string Province { get; set; } // Province de l'adresse
+    public required string Numero { get; set; } // Numéro de rue de l'adresse
+    public required string Rue { get; set; } // Rue de l'adresse
+    public required string Ville { get; set; } // Ville de l'adresse
+    public required string CodePostal { get; set; } // Code postal de l'adresse
+    public required string Province { get; set; } // Province de l'adresse
 }
